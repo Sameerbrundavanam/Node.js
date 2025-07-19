56b9866b58a824a2417a08cb0b106fb45b79e4c8
SameerBrundavanam <22a91a0514@aec.edu.in>
2025-07-19 13:11:17 +0530

    REST API Design Done

--- FIRST_REST_API/index.js
+++ FIRST_REST_API/index.js
@@ -4,6 +4,14 @@ const app = express();
 
 const users = require('./MOCK_DATA-2.json')
 
+const fs = require('fs')
+
+
+//This is a MiddleWare whic can be used as a plug-in
+//It deos this duty: Whenever i get the form data it tries to place in the body
+//It takes the data, makes it into a JS object adn places it in the req.body
+app.use(express.urlencoded({extended : false}));
+
 //Browsers by default make a GET request
 
 app.get("/users",(req,res) => {
@@ -27,18 +35,50 @@ app
 })
 
 .patch((req,res) => {
-     const id = Number(req.params.id);
-     return res.json({status : "pending"});
+    const id = Number(req.params.id);
+    const body = req.body;
+    const idx = users.findIndex(user => user.id === id);
+    if(idx === -1){
+        return res.json({status: "Failed"})
+    }
+    users[idx] = {...users[idx],...body};
+    fs.writeFile('./MOCK_DATA-2.json',JSON.stringify(users,null,2),(err,data) => {
+        if(err){
+            return res.json({status : "Error Occured"});
+        }
+        return res.json({status: "Success", data: users[idx]})
+    })
 })
 
 .delete((req,res) => {
-    return res.json({status : "pending"});
+    const id = Number(req.params.id);
+    const idx = users.findIndex(user => user.id === id);
+    if(idx === -1){
+        return res.json({status: "Failed"})
+    }
+    //splice -> (1,2) -> 1: index from which, 2: no.of records
+    const deletedUser = users.splice(idx,1)[0]
+    /*
+        JSON.stringify(users, null, 2):
+        Converts users to a string
+        null, 2 adds pretty formatting (indentation of 2 spaces)
+    */
+    fs.writeFile('./MOCK_DATA-2.json',JSON.stringify(users,null,2),(err,data) => {
+        if(err){
+            return res.json({status : "Error Occured"});
+        }
+        return res.json({status : "Success", deleteduser: deletedUser});
+    })
 });
 
 
 
 app.post("/api/users",(req,res) => {
-    return res.json({status : "pending"});
+    const body = req.body;
+    users.push({id: users.length+1, ...body})
+    fs.writeFile('./MOCK_DATA-2.json',JSON.stringify(users),(err,data) => {
+        return res.json({status : "Success", id: `${users.length}`});
+    })
 })
 
 
